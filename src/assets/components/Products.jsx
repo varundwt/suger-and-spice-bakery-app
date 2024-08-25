2854d30bad5d47cdf58dba4effd50595e5abfc9f
Varun Pandey <65996450+varundwt@users.noreply.github.com>
2024-08-25 17:46:11 +0530

    "formatted product page"

--- src/assets/components/Products.jsx
+++ src/assets/components/Products.jsx
@@ -1,41 +1,41 @@
 const productItems = [
   {
-    id:1,
+    id: 1,
     titleImage: "/images/cakes.jpg",
     title: "Cakes",
-    description : "Indulge in layers of delight with every slice."
+    description: "Indulge in layers of delight with every slice.",
   },
   {
-    id:2,
+    id: 2,
     titleImage: "/images/pastries.jpg",
     title: "Pastries",
-    description : "Flaky, buttery, and baked to perfection."
+    description: "Flaky, buttery, and baked to perfection.",
   },
   {
-    id:3,
+    id: 3,
     titleImage: "/images/snacks.jpg",
     title: "Snacks",
-    description : "Savory bites that satisfy your every craving."
+    description: "Savory bites that satisfy your every craving.",
   },
   {
-    id:4,
+    id: 4,
     titleImage: "/images/donuts.jpg",
     title: "Donuts",
-    description : "Sweet rings of joy, perfect for any time of day."
+    description: "Sweet rings of joy, perfect for any time of day.",
   },
   {
-    id:5,
+    id: 5,
     titleImage: "/images/bakery.jpg",
     title: "Bakery",
-    description : "Freshly baked wonders, crafted just for you."
+    description: "Freshly baked wonders, crafted just for you.",
   },
   {
-    id:6,
+    id: 6,
     titleImage: "/images/beverages.jpg",
     title: "Beverages",
-    description : "Refresh and recharge with every sip."
-  }
-]
+    description: "Refresh and recharge with every sip.",
+  },
+];
 
 export const Products = () => {
   return (
@@ -52,27 +52,29 @@ export const Products = () => {
           </h1>
         </div>
         <div className="flex flex-wrap gap-2 justify-around my-3 md:justify-center md:gap-20 lg:gap-5 lg:mx-0 lg:px-2 md:py-5 lg:py-6">
-        {productItems.map((item, i)=>(
-          <>
-          <div className="card glass md:w-72 lg:w-72 lg:mx-0 xl:w-96 m-1" key={i}>
-            <figure>
-              <img
-                src={item.titleImage}
-                alt="car!"
-                className="p-2 rounded-3xl"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">{item.title}</h2>
-              <p>{item.description}</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Learn now!</button>
+          {productItems.map((item, i) => (
+            <>
+              <div
+                className="card glass md:w-72 lg:w-72 lg:mx-0 xl:w-96 m-1"
+                key={i}
+              >
+                <figure>
+                  <img
+                    src={item.titleImage}
+                    alt="car!"
+                    className="p-2 rounded-3xl"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{item.title}</h2>
+                  <p>{item.description}</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Learn now!</button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          </>
-        ))}
-          
+            </>
+          ))}
         </div>
       </div>
     </>
